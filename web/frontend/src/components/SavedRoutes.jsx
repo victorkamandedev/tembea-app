741fccf3b96de40ad52c898711c55297247bab3e
victorkamandedev <paulvictorkamande@gmail.com>
2026-02-03 15:51:07 +0300

    changes to savedroutes url

--- web/frontend/src/components/SavedRoutes.jsx
+++ web/frontend/src/components/SavedRoutes.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+const API_URL = import.meta.env.VITE_API_URL;
 export default function SavedRoutes({ loadRoute }) {
   const [routes, setRoutes] = useState([]);
   const [loading, setLoading] = useState(true);

44221d36a3079c08112336c793da830c1922db80
victorkamandedev <paulvictorkamande@gmail.com>
2026-02-03 16:28:40 +0300

    changes to serverjs for mongo uri connection

--- web/backend/server.js
+++ web/backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // 🔌 Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
